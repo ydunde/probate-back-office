8b01c05434ca72b17ac91e8e6bf261bcc17caaec
RBaker <rebecca.baker@hmcts.net>
2019-04-24 16:17:04 +0100

    updated conversion script to blank user profile json and then revert back

--- ccdImports/conversionScripts/createAllXLS.sh
+++ ccdImports/conversionScripts/createAllXLS.sh
@@ -7,19 +7,14 @@ configFolder=${conversionFolder}/../configFiles
 
 if [ -z "$1" ]
   then
-    echo "Usage: ./ccdImports/conversionScripts/createAllXLS.sh CCD_DEF_CASE_SERVICE_BASE_URL CCD_PROD_FLAG"
-    echo "CCD_PROD_FLAG y to remove test users for prod config"
+    echo "Usage: ./ccdImports/conversionScripts/createAllXLS.sh CCD_DEF_CASE_SERVICE_BASE_URL"
     exit 1
 fi
 
 export CCD_DEF_CASE_SERVICE_BASE_URL=$1
-export CCD_PROD_FLAG=$2
 
-echo using url = CCD_PROD_FLAG
-
-if [ "$2" == "y" ] ;then
-    echo Prod Config = Yes
-    echo over writing test users
+if [[ "$1" == *"prod.internal"* ]] ;then
+    echo Removing test users
     cp ${configFolder}/UserProfile.json ${configFolder}/CCD_Probate_Backoffice/.
     cp ${configFolder}/UserProfile.json ${configFolder}/CCD_Probate_Caveat/.
     cp ${configFolder}/UserProfile.json ${configFolder}/CCD_Probate_Legacy_Cases/.
@@ -49,9 +44,8 @@ echo XLS files placed in /jsonToXLS folder
 #${binFolder}/ccd-import-definition.sh "../../xlsToJson/CCD_Probate_WillLodgement.xlsx"
 #${binFolder}/ccd-import-definition.sh "../../xlsToJson/CCD_Probate_StandingSearch.xlsx"
 
-if [ "$2" == "y" ] ;then
-    echo Prod Config = Yes
-    echo reverting user profiles
+if [[ "$1" == *"prod.internal"* ]] ;then
+    echo Reverting user profiles
     git checkout -- ${configFolder}/CCD_Probate_Backoffice/UserProfile.json
     git checkout -- ${configFolder}/CCD_Probate_Caveat/UserProfile.json
     git checkout -- ${configFolder}/CCD_Probate_Legacy_Cases/UserProfile.json
